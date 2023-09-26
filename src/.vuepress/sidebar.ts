cd6be0a4d0b0181474b7c8363763c89ad9ca10d5
Marius <m.gzegozevskis@gmail.com>
2023-09-27 01:06:42 +0300

    added go lang

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -16,6 +16,7 @@ export default sidebar({
                 "anotacijosirjavadocs",
                 "lambda",
                 "regex",
+                "kotlinjvm"
             ]
         },
         {
